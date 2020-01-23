babc9f9f5a9bbf5dff3b053c54f4c699632aa118
[Brandon] <desselle.brandon2@gmail.com>
2020-01-23 14:51:54 -0600

    Update recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,29 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    total = []
+
+    if len(ingredients) < len(recipe):
+        return 0
+
+    for i in recipe:
+        if math.floor((ingredients[i] / recipe[i])) == 0:
+            total.append(0)
+        else:
+            total.append(math.floor((ingredients[i] / recipe[i])))
+
+    if 0 in total:
+        return 0
+    else:
+        return min(total)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
